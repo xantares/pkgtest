1e5ab5bb27d9a8a30a64f61e74a7dbd75cffc9e2
xantares <xantares09@hotmail.com>
2013-09-21 10:06:58 +0200

    +qscintilla

---
+++ t_qscintilla.cxx
@@ -0,0 +1,31 @@
+#include "QtGui/qapplication.h"
+#include "QtGui/qpushbutton.h"
+#include "QtGui/qmainwindow.h"
+#include "QtCore/qtimer.h"
+#include <Qsci/qsciscintilla.h>
+
+class ExampleWindow : public QMainWindow
+{
+public:
+  ExampleWindow() : QMainWindow()
+  {
+    QsciScintilla * textEdit = new QsciScintilla;
+    textEdit->setText("Hello world!");
+    setCentralWidget(textEdit);
+
+//     QPushButton *hello = new QPushButton( "Hello world!", 0 );
+//     hello->resize( 100, 30 );
+    setCentralWidget( textEdit );
+  }
+};
+
+
+int main( int argc, char **argv )
+{
+  QApplication app( argc, argv );
+  QTimer::singleShot(1000, &app, SLOT(quit()));
+  ExampleWindow mainWin;
+  mainWin.show();
+  int exec = app.exec();
+  return exec;
+}
