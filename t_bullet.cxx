caa2590a1e9f2b9d07030e18a40a75dee734a76f
ljudert <ljudert@fujitsu-l64>
2013-11-20 21:54:08 +0100

    +openssl

--- t_bullet.cxx
+++ t_bullet.cxx
@@ -2,31 +2,31 @@
 #include <iostream>
 
 int main () {
-    std::cout << "Hello World!" << std::endl;
+  std::cout << "Hello World!" << std::endl;
 
-    // Build the broadphase
-    btBroadphaseInterface* broadphase = new btDbvtBroadphase();
+  // Build the broadphase
+  btBroadphaseInterface* broadphase = new btDbvtBroadphase();
 
-    // Set up the collision configuration and dispatcher
-    btDefaultCollisionConfiguration* collisionConfiguration = new btDefaultCollisionConfiguration();
-    btCollisionDispatcher* dispatcher = new btCollisionDispatcher(collisionConfiguration);
+  // Set up the collision configuration and dispatcher
+  btDefaultCollisionConfiguration* collisionConfiguration = new btDefaultCollisionConfiguration();
+  btCollisionDispatcher* dispatcher = new btCollisionDispatcher(collisionConfiguration);
 
-    // The actual physics solver
-    btSequentialImpulseConstraintSolver* solver = new btSequentialImpulseConstraintSolver;
+  // The actual physics solver
+  btSequentialImpulseConstraintSolver* solver = new btSequentialImpulseConstraintSolver;
 
-    // The world.
-    btDiscreteDynamicsWorld* dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher,broadphase,solver,collisionConfiguration);
-    dynamicsWorld->setGravity(btVector3(0,-10,0));
+  // The world.
+  btDiscreteDynamicsWorld* dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher,broadphase,solver,collisionConfiguration);
+  dynamicsWorld->setGravity(btVector3(0,-10,0));
 
-    // Do_everything_else_here
+  // Do_everything_else_here
 
 
-    // Clean up behind ourselves like good little programmers
-    delete dynamicsWorld;
-    delete solver;
-    delete dispatcher;
-    delete collisionConfiguration;
-    delete broadphase;
+  // Clean up behind ourselves like good little programmers
+  delete dynamicsWorld;
+  delete solver;
+  delete dispatcher;
+  delete collisionConfiguration;
+  delete broadphase;
 
-    return 0;
+  return 0;
 }
