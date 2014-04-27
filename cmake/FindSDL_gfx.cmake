89f7da1a09155d7b880037fdb66498821b318d85
xantares <xantares09@hotmail.com>
2014-04-27 17:07:24 +0200

    +sdl_gfx names

--- cmake/FindSDL_gfx.cmake
+++ cmake/FindSDL_gfx.cmake
@@ -1,8 +1,8 @@
 # - Locate SDL_gfx library
 # This module defines
-#  SDLGFX_LIBRARY, the library to link against
-#  SDLGFX_FOUND, if false, do not try to link to SDL
-#  SDLGFX_INCLUDE_DIR, where to find SDL/SDL_gfxPrimitives.h
+#  SDL_GFX_LIBRARY, the library to link against
+#  SDL_GFX_FOUND, if false, do not try to link to SDL
+#  SDL_GFX_INCLUDE_DIR, where to find SDL/SDL_gfxPrimitives.h
 #
 # $SDLDIR is an environment variable that would
 # correspond to the ./configure --prefix=$SDLDIR
@@ -16,9 +16,9 @@
 
 # On OSX, this will prefer the Framework version (if found) over others.
 # People will have to manually change the cache values of 
-# SDLGFX_LIBRARY to override this selection.
-FIND_PATH(SDLGFX_INCLUDE_DIR SDL_gfxPrimitives.h
-  $ENV{SDLGFXDIR}/include
+# SDL_GFX_LIBRARY to override this selection.
+FIND_PATH(SDL_GFX_INCLUDE_DIR SDL_gfxPrimitives.h
+  $ENV{SDL_GFXDIR}/include
   $ENV{SDLDIR}/include
   ~/Library/Frameworks/SDL_gfx.framework/Headers
   /Library/Frameworks/SDL_gfx.framework/Headers
@@ -38,30 +38,30 @@ FIND_PATH(SDLGFX_INCLUDE_DIR SDL_gfxPrimitives.h
 # unlikely that other Unix systems will find the framework path.
 # But if they do ([Next|Open|GNU]Step?), 
 # do they want the -framework option also?
-IF(${SDLGFX_INCLUDE_DIR} MATCHES ".framework")
+IF(${SDL_GFX_INCLUDE_DIR} MATCHES ".framework")
   # Extract the path the framework resides in so we can use it for the -F flag
-  STRING(REGEX REPLACE "(.*)/.*\\.framework/.*" "\\1" SDLGFX_FRAMEWORK_PATH_TEMP ${SDLGFX_INCLUDE_DIR})
-  IF("${SDLGFX_FRAMEWORK_PATH_TEMP}" STREQUAL "/Library/Frameworks"
-      OR "${SDLGFX_FRAMEWORK_PATH_TEMP}" STREQUAL "/System/Library/Frameworks"
+  STRING(REGEX REPLACE "(.*)/.*\\.framework/.*" "\\1" SDL_GFX_FRAMEWORK_PATH_TEMP ${SDL_GFX_INCLUDE_DIR})
+  IF("${SDL_GFX_FRAMEWORK_PATH_TEMP}" STREQUAL "/Library/Frameworks"
+      OR "${SDL_GFX_FRAMEWORK_PATH_TEMP}" STREQUAL "/System/Library/Frameworks"
       )
     # String is in default search path, don't need to use -F
-    SET(SDLGFX_LIBRARY "-framework SDL_gfx" CACHE STRING "SDL_gfx framework for OSX")
-  ELSE("${SDLGFX_FRAMEWORK_PATH_TEMP}" STREQUAL "/Library/Frameworks"
-      OR "${SDLGFX_FRAMEWORK_PATH_TEMP}" STREQUAL "/System/Library/Frameworks"
+    SET(SDL_GFX_LIBRARY "-framework SDL_gfx" CACHE STRING "SDL_gfx framework for OSX")
+  ELSE("${SDL_GFX_FRAMEWORK_PATH_TEMP}" STREQUAL "/Library/Frameworks"
+      OR "${SDL_GFX_FRAMEWORK_PATH_TEMP}" STREQUAL "/System/Library/Frameworks"
       )
     # String is not /Library/Frameworks, need to use -F
-    SET(SDLGFX_LIBRARY "-F${SDLGFX_FRAMEWORK_PATH_TEMP} -framework SDL_gfx" CACHE STRING "SDL_gfx framework for OSX")
-  ENDIF("${SDLGFX_FRAMEWORK_PATH_TEMP}" STREQUAL "/Library/Frameworks"
-    OR "${SDLGFX_FRAMEWORK_PATH_TEMP}" STREQUAL "/System/Library/Frameworks"
+    SET(SDL_GFX_LIBRARY "-F${SDL_GFX_FRAMEWORK_PATH_TEMP} -framework SDL_gfx" CACHE STRING "SDL_gfx framework for OSX")
+  ENDIF("${SDL_GFX_FRAMEWORK_PATH_TEMP}" STREQUAL "/Library/Frameworks"
+    OR "${SDL_GFX_FRAMEWORK_PATH_TEMP}" STREQUAL "/System/Library/Frameworks"
     )
   # Clear the temp variable so nobody can see it
-  SET(SDLGFX_FRAMEWORK_PATH_TEMP "" CACHE INTERNAL "")
+  SET(SDL_GFX_FRAMEWORK_PATH_TEMP "" CACHE INTERNAL "")
 
-ELSE(${SDLGFX_INCLUDE_DIR} MATCHES ".framework")
-  FIND_LIBRARY(SDLGFX_LIBRARY 
+ELSE(${SDL_GFX_INCLUDE_DIR} MATCHES ".framework")
+  FIND_LIBRARY(SDL_GFX_LIBRARY 
     NAMES SDL_gfx
     PATHS
-    $ENV{SDLGFXDIR}/lib
+    $ENV{SDL_GFXDIR}/lib
     $ENV{SDLDIR}/lib
     /usr/local/lib
     /usr/lib
@@ -70,9 +70,9 @@ ELSE(${SDLGFX_INCLUDE_DIR} MATCHES ".framework")
     /opt/csw/lib
     /opt/lib
     )
-ENDIF(${SDLGFX_INCLUDE_DIR} MATCHES ".framework")
+ENDIF(${SDL_GFX_INCLUDE_DIR} MATCHES ".framework")
 
 INCLUDE(FindPackageHandleStandardArgs)
 
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(SDLGFX REQUIRED_VARS SDLGFX_LIBRARY SDLGFX_INCLUDE_DIR)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(SDL_gfx REQUIRED_VARS SDL_GFX_LIBRARY SDL_GFX_INCLUDE_DIR)
 
