58e4dea51d18bbffa0145d98aa39b070611cf0fe
xantares <xantares09@hotmail.com>
2014-04-27 17:30:10 +0200

    +sdl2_image

---
+++ t_sdl2_image.c
@@ -0,0 +1,144 @@
+#include <stdio.h>
+#include <SDL.h>
+#include <SDL_image.h>
+// #include "asset.h"
+// #include "cleanup.h"
+
+/*
+ * Lesson 3: SDL Extension Libraries
+ */
+//Screen attributes
+const int SCREEN_WIDTH  = 640;
+const int SCREEN_HEIGHT = 480;
+//We'll be scaling our tiles to be 40x40
+const int TILE_SIZE = 40;
+
+/*
+ * Log an SDL error with some error message to the output stream of our choice
+ * @param os The output stream to write the message too
+ * @param msg The error message to write, format will be msg error: SDL_GetError()
+ */
+void logSDLError(char * msg) {
+  printf ("error: %s %s",msg, SDL_GetError());      
+}
+/*
+ * Loads an image into a texture on the rendering device
+ * @param file The image file to load
+ * @param ren The renderer to load the texture onto
+ * @return the loaded texture, or NULL if something went wrong.
+ */
+SDL_Texture* loadTexture(char * file, SDL_Renderer *ren){
+        SDL_Texture *texture = IMG_LoadTexture(ren, file);
+        if (texture == NULL){        
+                logSDLError("LoadTexture");
+        }
+        return texture;
+}
+/*
+ * Draw an SDL_Texture to an SDL_Renderer at position x, y, with some desired
+ * width and height
+ * @param tex The source texture we want to draw
+ * @param rend The renderer we want to draw too
+ * @param x The x coordinate to draw too
+ * @param y The y coordinate to draw too
+ * @param w The width of the texture to draw
+ * @param h The height of the texture to draw
+ */
+void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y, int w, int h){
+        //Setup the destination rectangle to be at the position we want
+        SDL_Rect dst;
+        dst.x = x;
+        dst.y = y;
+        dst.w = w;
+        dst.h = h;
+        SDL_RenderCopy(ren, tex, NULL, &dst);
+}
+/*
+ * Draw an SDL_Texture to an SDL_Renderer at position x, y, preserving
+ * the texture's width and height
+ * @param tex The source texture we want to draw
+ * @param rend The renderer we want to draw too
+ * @param x The x coordinate to draw too
+ * @param y The y coordinate to draw too
+ */
+void renderTexture_smart(SDL_Texture *tex, SDL_Renderer *ren, int x, int y){
+        int w, h;
+        SDL_QueryTexture(tex, NULL, NULL, &w, &h);
+        renderTexture(tex, ren, x, y, w, h);
+}
+
+int main(int argc, char** argv){
+        //Start up SDL and make sure it went ok
+        if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
+                logSDLError("SDL_Init");
+                return 1;
+        }
+
+        //Setup our window and renderer
+        SDL_Window *window = SDL_CreateWindow("Lesson 3", 100, 100, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        if (window == NULL){
+                logSDLError("CreateWindow");
+                SDL_Quit();
+                return 2;
+        }
+        SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+        if (renderer == NULL){
+                logSDLError("CreateRenderer");
+                SDL_DestroyWindow(window);
+                SDL_Quit();
+                return 3;
+        }
+
+        //The textures we'll be using
+        SDL_Texture *background = loadTexture("../test.jpg", renderer);
+        SDL_Texture *image = loadTexture("../test.jpg", renderer);
+        //Make sure they both loaded ok
+        if (background == NULL || image == NULL){
+                SDL_DestroyTexture(background);
+                SDL_DestroyTexture(image);
+                SDL_DestroyRenderer(renderer);
+                SDL_DestroyWindow(window);
+                
+                SDL_Quit();
+                return 4;
+        }
+
+        //Clear the window
+        SDL_RenderClear(renderer);
+
+        //Determine how many tiles we'll need to fill the screen
+        int xTiles = SCREEN_WIDTH / TILE_SIZE;
+        int yTiles = SCREEN_HEIGHT / TILE_SIZE;
+
+        //Draw the tiles by calculating their positions
+        int i;
+        for (i = 0; i < xTiles * yTiles; ++i){
+                int x = i % xTiles;
+                int y = i / xTiles;
+                renderTexture(background, renderer, x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+        }
+
+        //Draw our image in the center of the window
+        //We need the foreground image's width to properly compute the position
+        //of it's top left corner so that the image will be centered
+        int iW, iH;
+        SDL_QueryTexture(image, NULL, NULL, &iW, &iH);
+        int x = SCREEN_WIDTH / 2 - iW / 2;
+        int y = SCREEN_HEIGHT / 2 - iH / 2;
+        renderTexture_smart(image, renderer, x, y);
+
+        //Update the screen
+        SDL_RenderPresent(renderer);
+        SDL_Delay(5000);
+
+        //Destroy the various items
+        SDL_DestroyTexture(background);
+        SDL_DestroyTexture(image);
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        
+        IMG_Quit();
+        SDL_Quit();
+
+        return 0;
+}
