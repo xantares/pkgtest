dd878c3c92bdcc05a97fbad824dbd442b2097da2
xantares <xantares09@hotmail.com>
2015-07-25 10:14:55 +0200

    handle new hamt

--- t_hmat.c
+++ t_hmat.c
@@ -23,7 +23,17 @@
 // Cylinder
 #include <stdio.h>
 #include <math.h>
+#ifdef __cplusplus
+#include <complex>
+typedef std::complex<double> double_complex;
+#define make_complex(realPart, imagPart) \
+    std::complex<double>(realPart, imagPart)
+#else
 #include <complex.h>
+typedef double complex double_complex;
+#define make_complex(realPart, imagPart) \
+    realPart + imagPart * _Complex_I
+#endif
 
 #include "hmat/hmat.h"
 
@@ -43,34 +53,33 @@
     \param n number of points
     \return a vector of points.
  */
-DofCoordinate* createCylinder(double radius, double step, int n) {
-  DofCoordinate* result = (DofCoordinate*) malloc(n * sizeof(DofCoordinate));
+double* createCylinder(double radius, double step, int n) {
+  double* result = (double*) malloc(3 * n * sizeof(double));
   double length = 2 * M_PI * radius;
   int pointsPerCircle = length / step;
   double angleStep = 2 * M_PI / pointsPerCircle;
   int i;
   for (i = 0; i < n; i++) {
-    result[i].x = radius * cos(angleStep * i);
-    result[i].y = radius * sin(angleStep * i),
-    result[i].z = (step * i) / pointsPerCircle;
-    result[i].globalIndex = i;
+    result[3*i+0] = radius * cos(angleStep * i);
+    result[3*i+1] = radius * sin(angleStep * i),
+    result[3*i+2] = (step * i) / pointsPerCircle;
   }
   return result;
 }
 
 /** Write points into file. */
-void pointsToFile(DofCoordinate* points, int size, const char* filename) {
+void pointsToFile(double* points, int size, const char* filename) {
   int i;
   FILE * fp = fopen(filename, "w");
   for (i = 0; i < size; i++) {
-    fprintf(fp, "%e %e %e\n", points[i].x, points[i].y, points[i].z);
+    fprintf(fp, "%e %e %e\n", points[3*i], points[3*i+1], points[3*i+2]);
   }
   fclose(fp);
 }
 
 typedef struct {
   int n;
-  DofCoordinate* points;
+  double* points;
   double k;
 } problem_data_t;
 
@@ -81,10 +90,10 @@ typedef struct {
 void interaction_real(void* data, int i, int j, void* result)
 {
   problem_data_t* pdata = (problem_data_t*) data;
-  DofCoordinate* points = pdata->points;
-  double dx = points[i].x - points[j].x;
-  double dy = points[i].y - points[j].y;
-  double dz = points[i].z - points[j].z;
+  double* points = pdata->points;
+  double dx = points[3*i+0] - points[3*j+0];
+  double dy = points[3*i+1] - points[3*j+1];
+  double dz = points[3*i+2] - points[3*j+2];
   *((double*)result) = 1. / (sqrt(dx*dx + dy*dy + dz*dz) + 1e-10);
 }
 
@@ -94,37 +103,40 @@ void interaction_real(void* data, int i, int j, void* result)
 void interaction_complex(void* data, int i, int j, void* result)
 {
   problem_data_t* pdata = (problem_data_t*) data;
-  DofCoordinate* points = pdata->points;
+  double* points = pdata->points;
   double k = pdata->k;
-  double dx = points[i].x - points[j].x;
-  double dy = points[i].y - points[j].y;
-  double dz = points[i].z - points[j].z;
+  double dx = points[3*i+0] - points[3*j+0];
+  double dy = points[3*i+1] - points[3*j+1];
+  double dz = points[3*i+2] - points[3*j+2];
   double distance = sqrt(dx*dx + dy*dy + dz*dz) + 1e-10;
   double realPart = cos(k * distance) / (4 * M_PI * distance);
   double imagPart = sin(k * distance) / (4 * M_PI * distance);
-  *((double complex*)result) = realPart + imagPart * _Complex_I;
+  *((double_complex*)result) = make_complex(realPart, imagPart);
 }
 
 int main(int argc, char **argv) {
   double radius, step, k;
-  DofCoordinate* points;
+  double* points;
   hmat_settings_t settings;
   int n;
   char arithmetic;
-  void* cluster_tree;
-  void* hmatrix;
+  hmat_clustering_algorithm_t* clustering;
+  hmat_cluster_tree_t* cluster_tree;
+  hmat_matrix_t* hmatrix;
   hmat_interface_t hmat;
   hmat_value_t type;
+  hmat_info_t mat_info;
   int rc;
   problem_data_t problem_data;
-  //int is_parallel_run = 0;
+  hmat_admissibility_t * admissibilityCondition = hmat_create_admissibility_standard(3.0);
+  int is_parallel_run = 0;
 
   /*if (argc != 3) {
     fprintf(stderr, "Usage: %s n_points (S|D|C|Z)\n", argv[0]);
     return 1;
   }*/
 
-  //n = atoi(argv[1]);
+  // n = atoi(argv[1]);
   n = 1234;
 
   //arithmetic = argv[2][0];
@@ -151,7 +163,6 @@ int main(int argc, char **argv) {
   hmat_get_parameters(&settings);
   hmat_init_default_interface(&hmat, type);
   settings.compressionMethod = hmat_compress_aca_plus;
-  settings.admissibilityFactor = 3.;
 
   hmat_set_parameters(&settings);
   if (0 != hmat.init())
@@ -163,7 +174,7 @@ int main(int argc, char **argv) {
   printf("Generating the point cloud...\n");
 
   radius = 1.;
-  step = 1.75 * M_PI * radius / sqrt(n);
+  step = 1.75 * M_PI * radius / sqrt((double)n);
   k = 2 * M_PI / (10. * step); // 10 points / lambda
   points = createCylinder(radius, step, n);
   printf("done.\n");
@@ -174,9 +185,15 @@ int main(int argc, char **argv) {
   problem_data.points = points;
   problem_data.k = k;
 
-  cluster_tree = create_cluster_tree(points, n);
+  clustering = hmat_create_clustering_median();
+  cluster_tree = hmat_create_cluster_tree(points, 3, n, clustering);
+  hmat_delete_clustering(clustering);
   printf("ClusterTree node count = %d\n", hmat_tree_nodes_count(cluster_tree));
-  hmatrix = hmat.create_empty_hmatrix(cluster_tree, cluster_tree);
+  hmatrix = hmat.create_empty_hmatrix_admissibility(cluster_tree, cluster_tree,
+                                                    0, admissibilityCondition);
+  hmat_delete_admissibility(admissibilityCondition);
+  hmat.get_info(hmatrix, &mat_info);
+  printf("HMatrix node count = %d\n", mat_info.nr_block_clusters);
   if (type == HMAT_SIMPLE_PRECISION || type == HMAT_DOUBLE_PRECISION)
     rc = hmat.assemble_simple_interaction(hmatrix, &problem_data, interaction_real, 0);
   else
@@ -187,13 +204,15 @@ int main(int argc, char **argv) {
     return rc;
   }
 
-  rc = hmat.factor(hmatrix);
+  rc = hmat.factorize(hmatrix, hmat_factorization_lu);
   if (rc) {
     fprintf(stderr, "Error in factor, return code is %d, exiting...\n", rc);
     hmat.finalize();
     return rc;
   }
 
+  hmat.destroy(hmatrix);
+  hmat_delete_cluster_tree(cluster_tree);
   hmat.finalize();
   return rc;
 }
